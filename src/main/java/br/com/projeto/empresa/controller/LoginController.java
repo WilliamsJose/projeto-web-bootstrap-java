9d90f18d4d2ee3640bfc6bf2acdd2180147fc47d
Williams Jose <moflin02@gmail.com>
2020-03-31 19:56:57 -0300

    projeto refeito ainda sem o filter

---
+++ src/main/java/br/com/projeto/empresa/controller/LoginController.java
@@ -0,0 +1,50 @@
+package br.com.projeto.empresa.controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		
+		
+		if(email.equals("will@123.com") && senha.equals("123")){
+			request.setAttribute("email", email);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);
+		
+		}else {
+			request.setAttribute("erro", "Email ou senha incorretos!");
+			
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			rd.forward(request, response);
+		}
+		
+			
+	}
+
+}
